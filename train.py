6048755671f6e1abe722724625c3ecaa1ffc7bf7
Slava Chalnev <slava.chalnev@gmail.com>
2023-07-06 22:30:42 +1000

    tweak params

--- train.py
+++ train.py
@@ -15,7 +15,8 @@ from mlp import GeluMLP
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = HookedTransformer.from_pretrained_no_processing("pythia-70m-v0")
 
-text_data = load_dataset("NeelNanda/pile-10k", split="train")
+# text_data = load_dataset("NeelNanda/pile-10k", split="train")
+text_data = load_dataset("openwebtext", split="train[:10%]")
 text_dataset = tokenize_and_concatenate(text_data, model.tokenizer)
 
 model_dataset = ModelDataset(model, layer_idx=1, dataset=text_dataset, batch_size=8, device=device)
@@ -39,7 +40,7 @@ mlp4x = GeluMLP(
     ).to(device)
 
 mlps = [mlp1x, mlp2x, mlp4x]
-optimizers = [torch.optim.AdamW(mlp.parameters(), lr=1e-3) for mlp in mlps]
+optimizers = [torch.optim.AdamW(mlp.parameters(), lr=2e-4) for mlp in mlps]
 
 writer = SummaryWriter()
 
@@ -47,7 +48,7 @@ num_steps = 10000
 for batch_idx, (pre, post) in tqdm.tqdm(enumerate(model_dataset), total=num_steps):
     # pre and post have shape (batch_size * seq_len, d_model)
     # add noise to input
-    pre = pre + torch.rand_like(pre) * 1e-3
+    pre = pre + torch.rand_like(pre) * 1e-2
 
     for mlp, optimizer in zip(mlps, optimizers):
         optimizer.zero_grad()
