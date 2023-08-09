1764e9c76ddf0e9473039f66f2d8f3db0b6d52b2
Sviatoslav <slava.chalnev@gmail.com>
2023-08-09 10:48:51 +1000

    Don't pre-sort

--- activations.py
+++ activations.py
@@ -20,7 +20,7 @@ def analyse_feature(
         mlp_dir: str = 'mlps',
         layer=1,
         n_sequences=8000,
-        n_pre_sort=100,
+        # n_pre_sort=100,
         dataset_name='NeelNanda/pile-10k',
         mlp_dims=None,
     ):
@@ -81,36 +81,44 @@ def analyse_feature(
         ]
 
 
-    act_sums = [] # for every mlp we have a tensor of shape (h_size, 3)
-    for h_size in mlp_dims + [model.cfg.d_model * 4]:  # last one is for the original model.
-        act_sums.append(torch.zeros((h_size, 3)))
-    counts = torch.zeros((3,))
+    # act_sums = [] # for every mlp we have a tensor of shape (h_size, 3)
+    # for h_size in mlp_dims + [model.cfg.d_model * 4]:  # last one is for the original model.
+    #     act_sums.append(torch.zeros((h_size, 3)))
+    # counts = torch.zeros((3,))
 
-    with model.hooks(fwd_hooks=hooks), torch.no_grad():
-        for i, item in tqdm(enumerate(filtered_dataset)):
-            label_idx = label_to_idx[item['label']]
-            counts[label_idx] += 1
+    # with model.hooks(fwd_hooks=hooks), torch.no_grad():
+    #     for i, item in tqdm(enumerate(filtered_dataset)):
+    #         label_idx = label_to_idx[item['label']]
+    #         counts[label_idx] += 1
 
-            model.forward(item['tokens'].unsqueeze(0), stop_at_layer=layer+1)
+    #         model.forward(item['tokens'].unsqueeze(0), stop_at_layer=layer+1)
 
-            for mlp_i, mlp in enumerate(mlps):
-                h = mlp.forward(mlp_state['input'].to(device), return_post_act=True).squeeze(0).cpu()
-                act_sums[mlp_i][:, label_idx] += h
-            act_sums[-1][:, label_idx] += mlp_state['hidden'].squeeze(0).cpu() # for the original model
+    #         for mlp_i, mlp in enumerate(mlps):
+    #             h = mlp.forward(mlp_state['input'].to(device), return_post_act=True).squeeze(0).cpu()
+    #             act_sums[mlp_i][:, label_idx] += h
+    #         act_sums[-1][:, label_idx] += mlp_state['hidden'].squeeze(0).cpu() # for the original model
 
-    act_means = [act_sum / counts for act_sum in act_sums]
+    # act_means = [act_sum / counts for act_sum in act_sums]
 
     # get top n_pre_sort neurons by largest difference between ngram types
+    # top_neuron_idxs = []
+    # for act_mean in act_means:
+        # diffs = act_mean[:, 0] - torch.max(act_mean[:, 1], act_mean[:, 2])
+        # sorted_idx = diffs.argsort(descending=True)
+        # top_neuron_idxs.append(sorted_idx[:n_pre_sort].tolist())
+        # # print(diffs[top_neuron_idxs[-1]])
+    
     top_neuron_idxs = []
-    for act_mean in act_means:
-        diffs = act_mean[:, 0] - torch.max(act_mean[:, 1], act_mean[:, 2])
-        #top_neuron_idxs.append(diffs.argsort(descending=True)[:n_pre_sort])  # get top neurons
-        sorted_idx = diffs.argsort(descending=True)
-        top_neuron_idxs.append(sorted_idx[:n_pre_sort].tolist())
-        print(diffs[top_neuron_idxs[-1]])
+    # hack
+    for mlp in mlps:
+        # append all neurons
+        top_neuron_idxs.append(list(range(mlp.hidden_size)))
+    top_neuron_idxs.append(list(range(model.cfg.d_model * 4))) # for the original model
+
 
     # Prepare empty lists to store neuron activations
-    neuron_activations = [[[[] for _ in range(3)] for _ in range(n_pre_sort)] for _ in range(len(top_neuron_idxs))]
+    neuron_activations = [[[[] for _ in range(3)] for _ in range(mlp.hidden_size)] for mlp in mlps]
+    neuron_activations.append([[[] for _ in range(3)] for _ in range(model.cfg.d_model * 4)]) # for the original model
 
     with model.hooks(fwd_hooks=hooks), torch.no_grad():
         for i, item in tqdm(enumerate(filtered_dataset)):
@@ -129,11 +137,12 @@ def analyse_feature(
     return neuron_activations, top_neuron_idxs
 
 
-def rank_by_classifier(neuron_activations, top_neuron_idxs, n_pre_sort=100):
+def rank_by_classifier(neuron_activations, top_neuron_idxs):
     n_mlps = len(neuron_activations)
 
     # Training a classifier for each neuron and recording the accuracy, precision, and recall
-    classifier_metrics = [[{"accuracy": 0, "precision": 0, "recall": 0} for _ in range(n_pre_sort)] for _ in range(len(top_neuron_idxs))]
+    classifier_metrics = [[{"accuracy": 0, "precision": 0, "recall": 0} for _ in top_neuron_idxs[mlp_i]] for mlp_i in range(n_mlps)]
+
     for mlp_i in range(n_mlps):
         for neuron_i in range(len(top_neuron_idxs[mlp_i])):
             # Preparing the data
@@ -191,6 +200,7 @@ def plot_hist(neuron_activations, top_neuron_idxs, feature_name, mlp_dir='mlps',
     mlp_names = [str(dim) for dim in mlp_dims] + ['original']
 
     for i, neuron_label_activations in enumerate(neuron_activations):
+        print('len neuron_label_activations', len(neuron_label_activations))
         for j, label_activations in enumerate(neuron_label_activations):
             neuron_idx = top_neuron_idxs[i][j] # Get the actual neuron index
             for k, activations in enumerate(label_activations):
@@ -209,7 +219,7 @@ def plot_hist(neuron_activations, top_neuron_idxs, feature_name, mlp_dir='mlps',
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mlp-dir", default='mlps', type=str, help="Dir to load MLPs and save plots.")
+    parser.add_argument("--mlp-dir", type=str, required=True, help="Dir to load MLPs and save plots.")
     parser.add_argument("--mlp-type", default='gelu', type=str, help="Type of MLP to use. gelu or solu.")
     parser.add_argument('--mlp-dims', nargs='+', type=int, default=None, help='List of MLP dimensions')
     args = parser.parse_args()
@@ -222,7 +232,6 @@ if __name__ == "__main__":
         'mental-health',
         'side-effects',
         ]
-    n_pre_sort = 1000
     
     for feature_name in feature_names:
         print(f'Analysing {feature_name}')
@@ -231,8 +240,7 @@ if __name__ == "__main__":
                                                               mlp_dir=args.mlp_dir,
                                                               dataset_name='openwebtext',
                                                               mlp_dims=args.mlp_dims,
-                                                              n_pre_sort=n_pre_sort,
                                                               )
-        neuron_activations, top_neuron_idxs, classifier_metrics = rank_by_classifier(neuron_activations, top_neuron_idxs, n_pre_sort=n_pre_sort)
+        neuron_activations, top_neuron_idxs, classifier_metrics = rank_by_classifier(neuron_activations, top_neuron_idxs)
         print(classifier_metrics)
         plot_hist(neuron_activations, top_neuron_idxs, feature_name, mlp_dir=args.mlp_dir, mlp_dims=args.mlp_dims)
