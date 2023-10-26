617f9c7b986ca506dcfea5d1f8916bf7af406b02
Sviatoslav <slava.chalnev@gmail.com>
2023-10-26 13:26:26 +1100

    Refactor MLP

--- mlp.py
+++ mlp.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class MLP(nn.Module):
     def __init__(self, input_size: int, hidden_size: int, output_size: int):
         super().__init__()
@@ -20,57 +21,55 @@ class ReluMLP(MLP):
         super().__init__(input_size, hidden_size, output_size)
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, output_size)
-        self.activation = nn.ReLU()
+        self.act = nn.ReLU()
     
-    def forward(self, x, return_pre_act=False, return_post_act=False, hidden_noise=0.0):
+    def forward(self, x):
         x = self.fc1(x)
 
-        if return_pre_act:
-            return x
-        
-        if hidden_noise > 0.0:
-            x = x + torch.randn_like(h) * hidden_noise
-
-        x = self.activation(x)
-
-        if return_post_act:
-            return x
+        x = self.act(x)
 
         h = self.fc2(x)
         return h, x
     
     def encode(self, x):
         x = self.fc1(x)
-        x = self.activation(x)
+        x = self.act(x)
         return x
 
 
-class GeluMLP(nn.Module):
+class GeluMLP(MLP):
     def __init__(self, input_size: int, hidden_size: int, output_size: int):
-        super().__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
+        super().__init__(input_size, hidden_size, output_size)
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, output_size)
-        self.activation = nn.GELU()
+        self.act = nn.GELU()
     
-    def forward(self, x, return_pre_act=False, return_post_act=False, hidden_noise=0.0):
+    def forward(self, x):
         h = self.fc1(x)
-
-        if return_pre_act:
-            return h
         
-        if hidden_noise > 0.0:
-            h = h + torch.randn_like(h) * hidden_noise
-
-        h = self.activation(h)
-
-        if return_post_act:
-            return h
+        h = self.act(h)
 
         h = self.fc2(h)
         return h
+
+    def encode(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        return x
+
+
+class Identity(MLP):
+    def __init__(self, size):
+        super().__init__(size, size, size)
+    
+    def forward(self, x):
+        return x
+    
+    def encode(self, x):
+        return x
+
+
+##################
 
 
 def solu(x, temperature=1.0):
@@ -106,17 +105,4 @@ class SoluMLP(nn.Module):
         h = self.fc2(h)
         return h
 
-
-class Identity(nn.Module):
-    def __init__(self, size):
-        self.input_size = size
-        self.hidden_size = size
-        self.output_size = size
-        super().__init__()
-    
-    def forward(self, x):
-        return x
-    
-    def encode(self, x):
-        return x
     
